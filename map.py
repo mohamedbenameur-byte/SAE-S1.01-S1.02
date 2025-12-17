bd6aa72a8829071e885126408f69f9e135cca3b8
IsmaelBacheh <ismael.djib.69@gmail.com>
2025-12-17 14:55:42 +0100

    Add files via upload

---
+++ map.py
@@ -0,0 +1,14 @@
+
+l_map = [] #Cette liste contiendra ma map en 2D
+l_map = [["□"] * 16]*16 #Ajoute 10 colonnes de 10 entiers(int) ayant pour valeurs 0
+
+print(l_map)
+
+
+
+"""def info_carre()
+    
+
+
+
+def creer_map()"""
